7a39832824164e0728d59798ea49f2523b6d617b
NoName <noname@noname.com>
2026-01-07 14:55:58 +0200

    Fixed proving (partially)

--- acl2-utils.lisp
+++ acl2-utils.lisp
@@ -1,23 +1,55 @@
+;; :set-checkpoint-summary-limit 2000
+
 (in-package "ACL2")
 
 (include-book "utils")
 
-(def-pop-until pop-until-equal-zero some-item-zerop :criteria zerop)
+;; "pop-until-zero" is pop-until some item is zerop (predicate: equals to zero)
+(def-pop-until pop-until-zero some-item-zerop :criteria zerop)
 
-(defthm pop-until-zero-never-expands
-  (<= (len (pop-until-equal-zero stack include))
-      (len stack)))
+;; Pop never expands/increase size of a stack
+;; (defthm pop-until-zero--never-expands
+;;   (<= (len (pop-until-zero stack include))
+;;       (len stack)))
 
-(defthm pop-until-zero-shrinks-if-elem-exists
-  (implies (some-item-zerop stack)  ;; if item is in the stack
-           (< (len (pop-until-equal-zero stack t))
-              (len stack))))
+(defthm not-equal-nil-not-found
+    (not (equal nil :not-found))
+  :rule-classes nil)
 
-(defthm next-fname-start-multi-decorations
-    (implies (and (member-eq st '(:decorations :dash))
-                  (eql st ev))
-             (let* ((result (next-fname-start st ev))
-                    (new-st (car result))
-                    (ended-p (cdr result)))
-               (and (eql new-st st)
-                    (null ended-p)))))
+(defthm not-equal-cons-not-found
+  (implies (consp x)
+           (not (equal (cons a x) :not-found))))
+
+(defthm cdr-of-cons-not-not-found
+  (implies (true-listp stack)
+           (not (equal (cdr stack) :not-found))))
+
+;; If elem exists (see: some-item-zerop stack), the stack after pop shrinks
+(defthm pop-until-zero--shrinks-if-elem-exists
+    (implies (and (true-listp stack)
+                  (some-item-zerop stack))
+             (< (len (pop-until-zero stack t))
+                (len stack)))
+  :hints (("Goal" :use cdr-of-cons-not-not-found)))
+
+;; (defthm pop-until-zero--same-size-if-no-elem
+;;   (implies (not (some-item-zerop stack))
+;;            (= (len (pop-until-zero stack t))
+;;               (len stack))))
+
+;; BUGGY:
+;; (defthm pop-until-zero--same-size-if-no-elem
+;;   (implies (not (some-item-zerop stack))
+;;            (= (len (pop-until-zero stack stack t))
+;;               0)))
+
+;; Loops are in :decorations :dash only ("loops" mean don't stop, ie, ended-p is false and
+;; stays in the same state, ie new-st = st):
+;; (defthm next-fname--loops
+;;     (implies (and (member-eq st '(:decorations :dash))
+;;                   (eql st ev))
+;;              (let* ((result (next-fname st ev))
+;;                     (new-st (car result))
+;;                     (ended-p (cdr result)))
+;;                (and (eql new-st st)
+;;                     (null ended-p)))))
--- utils.lisp
+++ utils.lisp
@@ -1,22 +1,58 @@
 (in-package "ACL2")
 
+;; (defun make-symbol-like (base suffix)
+;;   #+acl2
+;;   (packn (list base suffix))
+;;   #-acl2
+;;   (intern (format nil "~A-~A" base suffix) (symbol-package base)))
+
 (defmacro def-pop-until (name pred-name &key criteria)
-  `(progn
-     (defun ,name (stack include)
-       (cond
-         ((endp stack) nil)
-         ((,criteria (car stack))
-          (if include (cdr stack) stack))
-         (t (,name (cdr stack) include))))
-     (defun ,pred-name (stack)
-       (cond
-         ((endp stack) nil)
-         ((,criteria (car stack)) t)
-         (t (,pred-name (cdr stack)))))))
+  "Defines a POP-UNTIL function and a predicate for checking the stopping condition.
+   Works in ACL2 (uses PACKN) and in plain Common Lisp (uses FORMAT/INTERN)."
+  (let ((rec-name
+          #+acl2
+            (packn (list name "-REC"))
+          #-acl2
+            (intern (format nil "~A-REC" name) (symbol-package name))))
+    `(progn
+       ;; Recursive helper
+       (defun ,rec-name (stack include)
+         (cond
+           ((endp stack) :not-found)
+           ((,criteria (car stack))
+            (if include (cdr stack) stack))
+           (t (,rec-name (cdr stack) include))))
+       ;; Top-level function
+       (defun ,name (stack include)
+         (let ((res (,rec-name stack include)))
+           (if (eql res :not-found) stack res)))
+       ;; Predicate function
+       (defun ,pred-name (stack)
+         (cond
+           ((endp stack) nil)
+           ((,criteria (car stack)) t)
+           (t (,pred-name (cdr stack))))))))
 
-;; (def-pop-until pop-until-equal-zero some-pop-until-equal-zero :criteria zerop)
 
-(defun next-fname-start (st ev)
+;; (defmacro def-pop-until (name pred-name &key criteria)
+;;   (let ((rec-name (make-symbol-like name 'rec)))
+;;     `(progn
+;;      (defun ,rec-name (orig-stack stack include)
+;;        (cond
+;;          ((endp stack) orig-stack)
+;;          ((,criteria (car stack))
+;;           (if include (cdr stack) stack))
+;;          (t (,name orig-stack (cdr stack) include))))
+;;      (defun ,name (stack include) (,rec-name stack stack include))
+;;      (defun ,pred-name (stack)
+;;        (cond
+;;          ((endp stack) nil)
+;;          ((,criteria (car stack)) t)
+;;          (t (,pred-name (cdr stack))))))))
+
+(def-pop-until pop-until-equal-zero some-pop-until-equal-zero :criteria zerop)
+
+(defun next-fname (st ev)
   (cond
     ((and (eql st :decorations) (eql ev :dash))
      (cons :dash nil))
